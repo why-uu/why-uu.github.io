fd855bb9b959cf5a623ac4f2c5755fcc0ff2d5e6
why-uu <sujattwahyu99@gmail.com>
2022-12-08 19:53:56 +0700

    first commit

---
+++ profil.php
@@ -0,0 +1,257 @@
+<!doctype html>
+<html lang="en">
+<div class="overflow-auto">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profil 19SA1224</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <nav class="navbar navbar-expand-lg bg-secondary ">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse  justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <style>
+      a {
+        color: black;
+      }
+
+      a:hover {
+        color: red;
+      }
+    </style>
+  </head>
+
+
+  <body>
+    <div class="h-100 p-5 bg-light rounded-3">
+      <img src="foto2.jpeg" class="rounded mx-auto d-block shadow-lg p-1 mb-1 bg-body rounded-circle" height="300px" width="300px">
+      <h2 style="text-align: center; color:black;" class="fs-4">SUJAT WAHYU SAPUTRO</h2>
+
+      <div class="text-center">
+        <i class="bi bi-envelope-fill d-inline "></i>
+        <a href="mailto:sujattwahyu99@gmail.com" target="_blank" style="text-decoration:none;">
+          <h6 class="d-inline ms-1 me-2 fw-light">sujattwahyu99@gmail.com</h6>
+        </a>
+        <i class="bi bi-telephone-fill"></i>
+        <a href=" https://wa.me/+6281233876990" target="_blank" style="text-decoration:none;">
+          <h6 class="d-inline ms-1 me-2 fw-light">+6281233876990</h6>
+        </a>
+        <i class="bi bi-geo-alt-fill"></i>
+        <a href="https://goo.gl/maps/feMmyXpFCDYBV97q7" target="_blank" style="text-decoration:none;">
+          <h6 class="d-inline ms-1 fw-light">Ds.Bangsa, Kec.Kebasen</h6>
+        </a>
+
+      </div>
+    </div>
+    <br />
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <img src="profil.png" class="card-img-top rounded mx-auto d-block" style="height: 200px; width: 200px;">
+          <div class="card-body">
+            <h5 class="card-title">Biodata</h5>
+            <p class="card-text">Berikut Biodata Saya</p>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item text-truncate">Nama : Sujat Wahyu Saputro</li>
+              <li class="list-group-item text-truncate">Nim : 19SA1224</li>
+              <li class="list-group-item text-truncate">Kls : IF19A</li>
+            </ul>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 2 days ago</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="sekolah.png" class="card-img-top rounded mx-auto d-block" style="height: 200px; width: 200px;">
+          <div class="card-body">
+            <h5 class="card-title">Akademik</h5>
+            <p class="card-text">Berikut History Akademik Saya</p>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item text-truncate">SD N 3 BANGSA</li>
+              <li class="list-group-item text-truncate">SMP N 2 KEBASEN</li>
+              <li class="list-group-item text-truncate">SMK N 2 BANYUMAS</li>
+            </ul>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 2 days ago</small>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card h-100">
+          <img src="hobby.png" class="card-img-top rounded mx-auto d-block" style="height: 200px; width: 200px;">
+          <div class="card-body">
+            <h5 class="card-title">Hobby</h5>
+            <p class="card-text">Berikut Hobby Saya</p>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item text-truncate">Nonton Film</li>
+              <li class="list-group-item text-truncate">Main Game</li>
+              <li class="list-group-item text-truncate">Editing Foto</li>
+            </ul>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 2 days ago</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br />
+
+
+    <div class="card">
+      <div class="card-body">
+        <h2 style="text-align: center;">SKILLS</h2>
+        <br />
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h5>Editing Video</h5>
+            </div>
+            <div class="col-10">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">70%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h5>Editing Foto </h5>
+            </div>
+            <div class="col-10">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">60%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h5>Speaking</h5>
+            </div>
+            <div class="col-10">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 40%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">40%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h5> Programming</h5>
+            </div>
+            <div class="col-10">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 20%;" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br />
+
+
+
+    <div class="card">
+      <div class="card-body">
+        <section id="popular-courses" class="courses">
+          <div class="container" data-aos="fade-up">
+            <div class="section-title">
+              <h2 style="text-align: center;">ALBUM</h2>
+              <br />
+            </div>
+            <div class="row" data-aos="zoom-in" data-aos-delay="100">
+
+              <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+                <div class="course-item">
+                  <img src="foto3.jpeg" class="img-fluid" style="height: 290px;">
+                  <div class="course-content">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                <div class="course-item">
+                  <img src="logoku.png" class="img-fluid" style="height: 290px;">
+                  <div class="course-content">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+                <div class="course-item">
+                  <img src="foto2.jpeg" class="img-fluid">
+                  <div class="course-content">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <br />
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+  <br />
+  <footer class="bg-secondary text-center text-lg-start">
+    <div class="text-center p-2" style="color:antiquewhite;">
+      © 2022 Copyright:
+      <a class="text-light bg-secondary" href="https://www.facebook.com/profile.php?id=100007097358496" target="_blank">19SA1224</a>
+    </div>
+  </footer>
+
+</div>
+
+</html>
